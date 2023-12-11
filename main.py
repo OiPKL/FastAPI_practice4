721d2b759997e4d638ecae9a2cfbfcc24d3dec37
dhkswls1998 <dhkswls1998@naver.com>
2023-12-11 14:52:20 +0900

    8081

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ app.include_router(router_garden.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
